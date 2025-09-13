054aa3d8b6c0bd992393665a3f5eb37b6913a89c
Alessandro Rodrigues <alessandro.melo01@fatec.sp.gov.br>
2025-09-12 21:11:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.send('Texto de conflito');
+  res.send('Tentando um conflito aqui');
 });
 
 app.get('/api/test', (req, res) => {
