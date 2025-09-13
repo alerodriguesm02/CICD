f895a60f2c2093fd619b8f491b025fcfa48e46f6
Alessandro Rodrigues <alessandro.melo01@fatec.sp.gov.br>
2025-09-12 21:02:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.jso());
 
 app.get('/api', (req, res) => {
   res.send('Alterei o texto do hello world.');
